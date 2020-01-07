041d2f2a798db4e1735b11936ebba1f50b614df5
hejingjing <hejingjing@sinosoft.com.cn>
2020-01-07 16:08:19 +0800

    20190107 commit

---
+++ springboot-email/src/main/java/com/springbootemail/service/impl/MailServiceImpl.java
@@ -0,0 +1,87 @@
+package com.springbootemail.service.impl;
+
+import com.springbootemail.service.MailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+
+@Service
+public class MailServiceImpl implements MailService {
+    @Autowired
+    private JavaMailSender mailSender;
+    @Value("${spring.mail.username}")
+    private String from;
+
+    @Override
+    public void sendSimpleMail(String to, String subject, String content, String... cc) {
+        SimpleMailMessage simpleMailMessage=new SimpleMailMessage();
+        simpleMailMessage.setFrom(from);
+        simpleMailMessage.setTo(to);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(content);
+        if (!StringUtils.isEmpty(cc)) {
+            simpleMailMessage.setCc(cc);
+        }
+        mailSender.send(simpleMailMessage);
+    }
+
+    @Override
+    public void sendHtmlMail(String to, String subject, String content, String... cc) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom(from);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(content, true);
+        if (!StringUtils.isEmpty(cc)) {
+            helper.setCc(cc);
+        }
+        mailSender.send(message);
+    }
+
+    @Override
+    public void sendAttachmentsMail(String to, String subject, String content, String filePath, String... cc) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom(from);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(content, true);
+        if (!StringUtils.isEmpty(cc)) {
+            helper.setCc(cc);
+        }
+        FileSystemResource file = new FileSystemResource(new File(filePath));
+        String fileName = filePath.substring(filePath.lastIndexOf(File.separator));
+        helper.addAttachment(fileName, file);
+
+        mailSender.send(message);
+    }
+
+    @Override
+    public void sendResourceMail(String to, String subject, String content, String rscPath, String rscId, String... cc) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom(from);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(content, true);
+        if (!StringUtils.isEmpty(cc)) {
+            helper.setCc(cc);
+        }
+        FileSystemResource res = new FileSystemResource(new File(rscPath));
+        helper.addInline(rscId, res);
+
+        mailSender.send(message);
+    }
+}
--- springboot-email/src/test/java/com/springbootemail/SpringbootEmailApplicationTests.java
+++ springboot-email/src/test/java/com/springbootemail/SpringbootEmailApplicationTests.java
@@ -1,13 +1,17 @@
 package com.springbootemail;
 
+import com.springbootemail.service.MailService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootEmailApplicationTests {
-
+    @Autowired
+    MailService mailService;
     @Test
     void contextLoads() {
+        mailService.sendSimpleMail("hejingjing@sinosoft.com.cn","美国暴雪娱乐有限公司","hello","hejingjing@sinosoft.com.cn");
     }
 
 }
